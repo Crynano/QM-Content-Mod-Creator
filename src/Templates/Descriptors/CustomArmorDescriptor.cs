98d00a231ca966a6ff78edc715ecc2e28d3ea0dc
Crynano <>
2026-03-16 20:34:38 +0100

    Added new custom armor descriptor

---
+++ src/Templates/Descriptors/CustomArmorDescriptor.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+
+namespace QM_ImporterAPI.Templates.Descriptors
+{
+    public class CustomArmorDescriptor : CustomItemContentDescriptor
+    {
+        public List<ArmorPartInfo> Parts { get; set; }
+
+        public CustomArmorDescriptor()
+        {
+
+        }
+
+        public static CustomArmorDescriptor GetExample(string id = null)
+        {
+            return new CustomArmorDescriptor
+            {
+                ItemId = id ?? "example_weaponid",
+                ImageProperties = new ImageProperties()
+                {
+                    IconSpriteIdOrPath = "Sprites/ExampleWeapon.png",
+                    SmallIconSpriteIdOrPath = "Sprites/ExampleWeaponSmall.png",
+                    ShadowOnFloorSpriteIdOrPath = "Sprites/ExampleWeaponShadow.png"
+                },
+                Parts = new List<ArmorPartInfo>()
+                {
+                    new ArmorPartInfo()
+                    {
+                        ArmorType = "ClothCommon",
+                        ArmorPart = "Hip",
+                        TextureIdOrPath = "Textures/ExampleTextureAtlas.png"
+                    },
+                    new ArmorPartInfo()
+                    {
+                        ArmorType = "ClothCommon",
+                        ArmorPart = "RThigh",
+                        TextureIdOrPath = "Textures/ExampleTextureAtlas.png"
+                    },
+                    new ArmorPartInfo()
+                    {
+                        ArmorType = "ClothCommon",
+                        ArmorPart = "LThigh",
+                        TextureIdOrPath = "Textures/ExampleTextureAtlas.png"
+                    },
+                    new ArmorPartInfo()
+                    {
+                        ArmorType = "ClothCommon",
+                        ArmorPart = "RLeg",
+                        TextureIdOrPath = "Textures/ExampleTextureAtlas.png"
+                    },
+                    new ArmorPartInfo()
+                    {
+                        ArmorType = "ClothCommon",
+                        ArmorPart = "LLeg",
+                        TextureIdOrPath = "Textures/ExampleTextureAtlas.png"
+                    },
+                }
+            };
+        }
+    }
+
+    public class ArmorPartInfo
+    {
+        public string ArmorType { get; set; }
+        public string ArmorPart { get; set; }
+        public string TextureIdOrPath { get; set; }
+    }
+}
